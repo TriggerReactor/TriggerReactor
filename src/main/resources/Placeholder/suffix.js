518afb88e5764f4a8779ea4f7b58a59fbcdf92ce
wysohn <wysohn2002@naver.com>
2018-03-10 15:54:12 -0800

    $prefix and $suffix

---
+++ src/main/resources/Placeholder/suffix.js
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ *     Copyright (C) 2018 wysohn
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+function suffix(args){
+	if(player == null)
+		return null;
+
+	https://github.com/MilkBowl/VaultAPI/blob/8dc0859cba3eee3263209d88999dce23597f26ad/src/main/java/net/milkbowl/vault/chat/Chat.java#L182
+	return vault.chat().getPlayerSuffix(player);
+}
