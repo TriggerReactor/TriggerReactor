51b2f9e80d3c1fd0a8a5914d33cd8c8f3de8b996
RedLime <redlimerl@gmail.com>
2018-11-22 01:46:59 +0900

    Add files via upload

--- src/main/resources/Placeholder/Bukkit/blockname.js
+++ src/main/resources/Placeholder/Bukkit/blockname.js
@@ -1,15 +1,15 @@
 function blockname(args){
 	
 	if ((args.length == 0) || (args.length == 1) || (args.length == 2) || (args.length == 3)) {
-		throw new Error("$blockname 플레이스 홀더는 $blockname:\"월드이름\":x:y:z 로 사용해야 합니다.");
+		throw new Error("$blockname placeholder should be used as $blockname:\"worldname\": x: y: z.");
 	}else if (args.length == 4) {
 		
 		if (Bukkit.getWorld(args[0]) == null) {
-			throw new Error("해당 월드가 존재하지 않습니다.");
+			throw new Error("The world does not exist.");
 			return null;
 		}
 		if ((typeof args[1] !== "number")||(typeof args[2] !== "number")||(typeof args[3] !== "number")) {
-			throw new Error("좌표값은 정수여야 합니다.");
+			throw new Error("Coordinate values must be integers.");
 			return null;
 		}
 		world = Bukkit.getWorld(args[0]);
