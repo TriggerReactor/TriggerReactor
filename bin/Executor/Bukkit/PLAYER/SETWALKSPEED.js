90943a8c3d99cbce20b737ada9985663565e08e6
gerzytet <gerzytet@gmail.com>
2019-06-23 22:10:55 -0400

    fix range check

--- bin/Executor/Bukkit/PLAYER/SETWALKSPEED.js
+++ bin/Executor/Bukkit/PLAYER/SETWALKSPEED.js
@@ -12,9 +12,9 @@ function SETWALKSPEED(args)
     throw new Error("Invalid argument for SETWALKSPEED: " + arg)
   }
   
-  if (arg < 0)
+  if (arg > 1 || arg < -1)
   {
-    throw new Error("Argument for Executor SETWALKSPEED should not be negative")
+    throw new Error("Argument for Executor SETWALKSPEED is outside of the allowable range -1..1")
   }
   
   player.setWalkSpeed(arg)
