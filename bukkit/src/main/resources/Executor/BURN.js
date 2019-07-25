1e00e6fa786fd6958a75b18c1e7e03767e4de4fd
Craig White <gerzytet@gmail.com>
2019-07-25 00:51:36 -0400

    null check here, type check there, range checks, length checks everywhere! added some more validation for executors fixed a bug in the process - #EXPLOSION claims in the docs that it has an arg that can casue a fiery explosion, but it turns out the arg isn't used.  It works as intended now.

--- bukkit/src/main/resources/Executor/BURN.js
+++ bukkit/src/main/resources/Executor/BURN.js
@@ -15,22 +15,53 @@
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************/
 function BURN(args){
-	if(args.length == 1){
-		var seconds = args[0];
+	if(args.length === 1){
+		if (typeof args[0] !== "number")
+		{
+			throw new Error("Invalid number for seconds to burn: " + args[0])
+		}
 		
-		player.setFireTicks(seconds * 20);
-	}else if(args.length == 2){ 
+		if (args[0] < 0)
+		{
+			throw new Error("The number of seconds to burn should be positive")
+		}
+		
+		player.setFireTicks(Math.round(args[0] * 20));
+	}else if(args.length === 2){ 
 		var entity = args[0];
-		var seconds = args[1];
 
-		if(typeof entity == "string"){
+		if (entity === null)
+		{
+			throw new Error("player to burn should not be null")
+		}
+		
+		if(typeof entity === "string"){
 			entity = Bukkit.getPlayer(entity);
+			
+			if (entity === null)
+			{
+				throw new Error("player to burn does not exist")
+			}
+		}
+		else if (!(entity instanceof Java.type("org.bukkit.entity.Entity")))
+		{
+			throw new Error("invalid entity to burn: " + entity)
+		}
+		
+		if (typeof args[1] !== "number")
+		{
+			throw new Error("The number of seconds to burn should be a number")
+		}
+		
+		if (args[1] < 0)
+		{
+			throw new Error("The number of seconds to burn should be positive")
 		}
 
-		entity.setFireTicks(seconds * 20);
+		entity.setFireTicks(Math.round(args[1] * 20));
 	}else {
 		throw new Error(
-			'Invalid parameters. Need [Number] or [Entity<entity or string>, Number]');
+			'Invalid number of parameters. Need [Number] or [Entity<entity or string>, Number]');
 	}
 	return null;
 }
