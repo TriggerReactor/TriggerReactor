8ac2682892a98eaee924fb1c9c7b2422413fe781
Craig White <gerzytet@gmail.com>
2019-08-02 00:14:44 -0400

    remove assertError methods form AbstractTestJavaScripts, the methods should only be in one, public, place

--- bukkit/src/test/java/js/AbstractTestJavaScripts.java
+++ bukkit/src/test/java/js/AbstractTestJavaScripts.java
@@ -87,44 +87,4 @@ public abstract class AbstractTestJavaScripts {
         engine.put("Temp", clazz);
         engine.eval("var "+clazz.getSimpleName()+" = Temp.static;");
     }
-
-    //assert that a runnable threw an error
-    @SuppressWarnings("unused")
-    protected static void assertError(ErrorProneRunnable run)
-    {
-        try {
-            run.run();
-        }
-        catch (Exception e) {
-            return;
-        }
-        Assert.fail("runnable did not throw any exception");
-    }
-
-    //assert that a runnable threw an error message with the content Error: + expectedMessage
-    protected static void assertError(ErrorProneRunnable run, String expectedMessage)
-    {
-        try {
-            assertError(run, message -> message.equals("Error: " + expectedMessage));
-        } catch (AssertionError e) {
-            if (e.getMessage().equals("runnable did not throw any exception")) {
-                throw e;
-            } else {
-                Assert.fail(e.getMessage() + ", expected: \"" + expectedMessage + "\"");
-            }
-        }
-    }
-
-    //assert that a runnable threw an error message that matches the predicate
-    protected static void assertError(ErrorProneRunnable run, Predicate<String> messageTest)
-    {
-        try {
-            run.run();
-        }
-        catch (Exception e) {
-            if (messageTest.test(e.getCause().getMessage())) return;
-            Assert.fail("Exeption message predicate failed to match message: \"" + e.getCause().getMessage() + "\"");
-        }
-        Assert.fail("runnable did not throw any exception");
-    }
 }
