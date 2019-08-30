3158295e26ad9ad415b9ff11e5fae07a2dd6e218
Craig White <gerzytet@gmail.com>
2019-08-29 21:46:11 -0400

    apply more validation to sponge executors

--- sponge/src/main/resources/Executor/BURN.js
+++ sponge/src/main/resources/Executor/BURN.js
@@ -15,7 +15,7 @@
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************/
 function BURN(args){
-	if(args.length == 1) {
+	if(args.length === 1) {
 		if (typeof args[0] !== "number") {
 			throw new Error("Invalid number for seconds to burn: " + args[0])
 		}
@@ -25,15 +25,26 @@ function BURN(args){
 		var seconds = args[0] * 20;
 		
 		player.offer(Keys.FIRE_TICKS, Math.round(seconds));
-	}else if(args.length == 2){ 
+	}else if(args.length === 2){ 
 		var entity = args[0];
-		var seconds = args[1] * 20;
 
-		if(typeof entity == "string"){
+		if (entity !== null) {
+			yhrow new Error("player to burn should not be null");
+		}
+		if(typeof entity === "string"){
 			entity = Sponge.getServer().getPlayer(entity);
 		}
+		if (entity === null) {
+			throw new Error("player to burn does not exist");
+		}
+		if (typeof args[1] !== "number") {
+			throw new Error("Invalid number for seconds to burn: " + args[0])
+		}
+		if (args[1] < 0) {
+			throw new Error("The number of seconds to burn should be positive")
+		}
 
-		entity.offer(Keys.FIRE_TICKS, seconds.intValue());
+		entity.offer(Keys.FIRE_TICKS, Math.round(seconds));
 	}else {
 		throw new Error(
 			'Invalid parameters. Need [Number] or [Entity<entity or string>, Number]');
