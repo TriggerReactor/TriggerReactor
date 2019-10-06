adf61073351b9b39f69971b737b39889d04026da
Craig White <gerzytet@gmail.com>
2019-10-05 23:27:17 -0400

    remove JsExecutorAlias

--- core/src/test/java/js/ExecutorTest.java
+++ core/src/test/java/js/ExecutorTest.java
@@ -6,30 +6,13 @@ import io.github.wysohn.triggerreactor.tools.timings.Timings;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Map;
 
 public class ExecutorTest extends JsTest{
-	//I had to include this to get around access restrictions.  Plz don't kill me wysohn
-	//So what does this do? I looked at the history, but it doesn't seem to do anything. Can you elaborate?
-	private class JSExecutorAlias extends JSExecutor {
-		public JSExecutorAlias(String executorName, ScriptEngine engine, InputStream stream)
-				throws ScriptException, IOException {
-			super(executorName, engine, stream);
-		}
-		
-		@Override
-		public Integer execute(Timings.Timing timing, boolean sync, Map<String, Object> variables, Object e,
-                Object... args) throws Exception {
-			return super.execute(Timings.LIMBO, sync, variables, e, args);
-		}
-	}
-	
-	private final JSExecutorAlias executor;
+	private final JSExecutor executor;
 	
 	public ExecutorTest(ScriptEngine engine, String name, String... directories) throws ScriptException, IOException {
 		super(engine, name, "Executor", directories);
-		executor = new JSExecutorAlias(name, engine, stream);
+		executor = new JSExecutor(name, engine, stream);
 	}
 
 	@Override
