8b319d676b1c678b143f70d7e4af931286468910
wysohn <wysohn2002@naver.com>
2019-12-13 18:08:47 -0800

    Refactoring Validator
    @gerzytet
    Let me know if you don't like something

--- core/src/main/java/io/github/wysohn/triggerreactor/core/script/validation/option/NameOption.java
+++ core/src/main/java/io/github/wysohn/triggerreactor/core/script/validation/option/NameOption.java
@@ -1,22 +1,14 @@
 package io.github.wysohn.triggerreactor.core.script.validation.option;
 
 public class NameOption extends ValidationOption {
-	static {
-		ValidationOption.register(new NameOption(), "name");
-	}
-	
-	static void init() {}
-	
-	private NameOption() {}
+    @Override
+    public boolean canContain(Object arg) {
+        return arg instanceof String;
+    }
 
-	@Override
-	public boolean canContain(Object arg) {
-		return arg instanceof String;
-	}
-
-	@Override
-	public String validate(Object arg, Object value) {
-		return null;
-	}
+    @Override
+    public String validate(Object arg, Object value) {
+        return null;
+    }
 
 }
--- core/src/test/java/io/github/wysohn/triggerreactor/core/script/validation/TestOptions.java
+++ core/src/test/java/io/github/wysohn/triggerreactor/core/script/validation/TestOptions.java
@@ -1,39 +1,62 @@
 package io.github.wysohn.triggerreactor.core.script.validation;
 
-import io.github.wysohn.triggerreactor.core.script.validation.option.MinimumOption;
-import io.github.wysohn.triggerreactor.core.script.validation.option.TypeOption;
-import io.github.wysohn.triggerreactor.core.script.validation.option.ValidationOption;
+import io.github.wysohn.triggerreactor.core.script.validation.option.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TestOptions {
-	public TestOptions() {}
-	
+	public TestOptions() {
+	}
+
+	private ValidationOptions validationOptions;
+
+	@Before
+	public void init() {
+		validationOptions = new ValidationOptionsBuilder()
+				.addOption(new MinimumOption(), "minimum")
+				.addOption(new MaximumOption(), "maximum")
+				.addOption(new NameOption(), "name")
+				.addOption(new TypeOption(), "type")
+				.build();
+	}
+
 	@Test
 	public void testSanity() {
-		assertEquals(ValidationOption.forName("type").getClass(), TypeOption.class);
-		assertEquals(ValidationOption.forName("minimum").getClass(), MinimumOption.class);
-		assertEquals(ValidationOption.forName("kjfdsdjjfjfffff()#@##"), null);
+		assertEquals(validationOptions.forName("type").getClass(), TypeOption.class);
+		assertEquals(validationOptions.forName("minimum").getClass(), MinimumOption.class);
+		assertEquals(validationOptions.forName("maximum").getClass(), MaximumOption.class);
+		assertEquals(validationOptions.forName("name").getClass(), NameOption.class);
+		assertEquals(validationOptions.forName("kjfdsdjjfjfffff()#@##"), null);
 	}
-	
+
 	@Test
 	public void testMinimum() {
-		ValidationOption min = ValidationOption.forName("minimum");
+		ValidationOption min = validationOptions.forName("minimum");
 		assertTrue(min.canContain(3));
 		assertEquals(min.validate(0, 5), null);
 		assertFalse(min.canContain("derp"));
 		assertNotEquals(min.validate(0, -1), null);
 	}
-	
+
+	@Test
+	public void testMaximum() {
+		ValidationOption max = validationOptions.forName("maximum");
+		assertTrue(max.canContain(23));
+		assertEquals(max.validate(99, 5), null);
+		assertFalse(max.canContain("derp2"));
+		assertNotEquals(max.validate(5, 12), null);
+	}
+
 	@Test
 	public void testType() {
-		ValidationOption type = ValidationOption.forName("type");
+		ValidationOption type = validationOptions.forName("type");
 		assertTrue(type.canContain("int"));
 		assertTrue(type.canContain("string"));
 		assertTrue(type.canContain("boolean"));
 		assertFalse(type.canContain("oOoOoOof"));
-		
+
 		assertEquals(type.validate("int", 6), null);
 		assertEquals(type.validate("int", 6.0), null);
 		assertEquals(type.validate("int", 0), null);
