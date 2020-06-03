5442264f8d0b0112b32668d60ee2a0c48d31b468
wysohn <wysohn2002@naver.com>
2020-06-03 00:08:17 -0700

    simplify the message

--- core/src/main/java/io/github/wysohn/triggerreactor/core/manager/config/InvalidTrgConfigurationException.java
+++ core/src/main/java/io/github/wysohn/triggerreactor/core/manager/config/InvalidTrgConfigurationException.java
@@ -2,6 +2,6 @@ package io.github.wysohn.triggerreactor.core.manager.config;
 
 public class InvalidTrgConfigurationException extends Exception {
     public InvalidTrgConfigurationException(String s, IConfigSource configSource) {
-        super(s + " caused by " + configSource);
+        super(configSource + ": " + s);
     }
 }
