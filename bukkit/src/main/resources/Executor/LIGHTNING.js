d9f760f5542a8cb0be283159868a698d62994c81
chickiyeah <55694044+chickiyeah@users.noreply.github.com>
2021-05-08 11:52:01 +0900

    Update LIGHTNING.js

--- bukkit/src/main/resources/Executor/LIGHTNING.js
+++ bukkit/src/main/resources/Executor/LIGHTNING.js
@@ -30,19 +30,11 @@ function LIGHTNING(args){
 	}
 	if(args.length ===
 		4){
-	    if(typeof args[0] !==
-			"string"
-	    	|| typeof args[1] !==
-				"number"
-	    	|| typeof args[2] !==
-					"number"
-	     	|| typeof args[3] !==
-						"number")
+	    if(typeof args[0] !== "string" || typeof args[1] !== "number" || typeof args[2] !== "number" || typeof args[3] !== "number")
 	    	throw new Error("Invalid parameters! [String, Number, Number, Number] or [Location]");
 		
 	    var world = Bukkit.getWorld(args[0]);
-	    if(world ===
-			null)
+	    if(world === null)
 	    	throw new Error("Unknown world named "+args[0]);
 	
 	    var Location = Java.type('org.bukkit.Location');
