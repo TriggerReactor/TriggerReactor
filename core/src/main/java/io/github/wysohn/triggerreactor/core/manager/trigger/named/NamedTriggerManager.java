49843ae4df27239568696d5c1b98b8bff6ce1e6e
wysohn <wysohn2002@naver.com>
2022-09-17 19:39:37 -0700

    named trigger tests

--- core/src/main/java/io/github/wysohn/triggerreactor/core/manager/trigger/named/NamedTriggerManager.java
+++ core/src/main/java/io/github/wysohn/triggerreactor/core/manager/trigger/named/NamedTriggerManager.java
@@ -16,67 +16,20 @@
  *******************************************************************************/
 package io.github.wysohn.triggerreactor.core.manager.trigger.named;
 
-import io.github.wysohn.triggerreactor.core.config.InvalidTrgConfigurationException;
-import io.github.wysohn.triggerreactor.core.config.source.ConfigSourceFactory;
-import io.github.wysohn.triggerreactor.core.config.source.IConfigSource;
 import io.github.wysohn.triggerreactor.core.main.TriggerReactorCore;
 import io.github.wysohn.triggerreactor.core.manager.trigger.AbstractTriggerManager;
 import io.github.wysohn.triggerreactor.core.manager.trigger.ITriggerLoader;
-import io.github.wysohn.triggerreactor.core.manager.trigger.TriggerInfo;
-import io.github.wysohn.triggerreactor.tools.FileUtil;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public final class NamedTriggerManager extends AbstractTriggerManager<NamedTrigger> {
 
-    public NamedTriggerManager(TriggerReactorCore plugin) {
-        super(plugin, new File(plugin.getDataFolder(), "NamedTriggers"), new ITriggerLoader<NamedTrigger>() {
-            private File[] getAllFiles(List<File> list, File file){
-                if(file.isDirectory()){
-                    File[] files = file.listFiles();
-                    if(files != null){
-                        for(File each : files){
-                            getAllFiles(list, each);
-                        }
-                    }
-                } else {
-                    list.add(file);
-                }
-
-                return list.toArray(new File[0]);
-            }
-
-            @Override
-            public TriggerInfo[] listTriggers(File folder, ConfigSourceFactory fn) {
-                File[] files = getAllFiles(new ArrayList<>(), folder);
-                return Arrays.stream(files)
-                        .filter(file -> file.getName().endsWith(".trg"))
-                        .map(file -> {
-                            String name = TriggerInfo.extractName(file);
-                            IConfigSource config = fn.create(folder, name);
-                            return new NamedTriggerInfo(folder, file, config);
-                        }).toArray(NamedTriggerInfo[]::new);
-            }
-
-            @Override
-            public NamedTrigger load(TriggerInfo info) throws InvalidTrgConfigurationException {
-                try {
-                    String script = FileUtil.readFromFile(info.getSourceCodeFile());
-                    return new NamedTrigger(info, script);
-                } catch (TriggerInitFailedException | IOException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-
-            @Override
-            public void save(NamedTrigger trigger) {
-                // we don't save NamedTrigger
-            }
-        });
+    public NamedTriggerManager(TriggerReactorCore plugin, ITriggerLoader<NamedTrigger> loader) {
+        super(plugin, new File(plugin.getDataFolder(), "NamedTriggers"), loader);
     }
+
+    public NamedTriggerManager(TriggerReactorCore plugin){
+        this(plugin, new NamedTriggerLoader());
+    }
+
 }
