cc496fad8e57b377c6b3a91a7e1bbe49c455d8c4
wysohn <wysohn2002@naver.com>
2022-09-24 19:23:21 -0700

    show warning if duplicated key is found

--- core/src/main/java/io/github/wysohn/triggerreactor/core/manager/trigger/AbstractTriggerManager.java
+++ core/src/main/java/io/github/wysohn/triggerreactor/core/manager/trigger/AbstractTriggerManager.java
@@ -87,8 +87,22 @@ public abstract class AbstractTriggerManager<T extends Trigger> extends Manager 
         try {
             T trigger = loader.load(info);
             put(triggerName, trigger);
+
+            checkDuplicatedKeys(info);
         } catch (Exception e) {
             throw new RuntimeException("Failed to load " + info, e);
+        }
+    }
+
+    private void checkDuplicatedKeys(TriggerInfo info) {
+        if(info == null)
+            return;
+
+        for(TriggerConfigKey key : TriggerConfigKey.values()){
+            if(info.hasDuplicate(key)){
+                plugin.getLogger().warning("Duplicated key found in " + info + ": " + key);
+                plugin.getLogger().warning(String.format("Key %s is deprecated and is now %s", key.getOldKey(), key.getKey()));
+            }
         }
     }
 
