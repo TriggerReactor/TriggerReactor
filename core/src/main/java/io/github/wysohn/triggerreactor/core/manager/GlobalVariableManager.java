a544bea9f3ea6d71a35bdedbce4eb8267a462d2c
wysohn <wysohn2002@naver.com>
2022-11-27 17:51:07 -0800

    simply delegate to remove() if value is null (#536)

--- core/src/main/java/io/github/wysohn/triggerreactor/core/manager/GlobalVariableManager.java
+++ core/src/main/java/io/github/wysohn/triggerreactor/core/manager/GlobalVariableManager.java
@@ -1,19 +1,19 @@
-/*******************************************************************************
- *     Copyright (C) 2018 wysohn
+/*
+ * Copyright (C) 2022. TriggerReactor Team
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *******************************************************************************/
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.github.wysohn.triggerreactor.core.manager;
 
 import io.github.wysohn.triggerreactor.core.config.IMigratable;
@@ -155,6 +155,11 @@ public class GlobalVariableManager extends Manager implements IMigratable {
 
         @Override
         public Object put(Object key, Object value) {
+            if (value == null) {
+                remove(key);
+                return null;
+            }
+
             if (key instanceof String) {
                 try {
                     GlobalVariableManager.this.put((String) key, value);
