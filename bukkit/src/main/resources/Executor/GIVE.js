9ef537e73f1c6cf0296d5fc7b416a1f1f920b8a0
Cupelt <guswnd8800@naver.com>
2023-05-18 11:37:57 +0900

    fixed CodeFactor issues

--- bukkit/src/main/resources/Executor/GIVE.js
+++ bukkit/src/main/resources/Executor/GIVE.js
@@ -16,34 +16,34 @@
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************/
 function GIVE(args){
-	if(args.length == 1){
+    if(args.length == 1){
 
-		if (!(args[0] instanceof Java.type("org.bukkit.inventory.ItemStack")))
-		{
-			throw new Error("Invalid ItemStack: " + args[0]);
-		}
+        if (!(args[0] instanceof Java.type("org.bukkit.inventory.ItemStack")))
+        {
+            throw new Error("Invalid ItemStack: " + args[0]);
+        }
 
-		if (!(player instanceof Java.type("org.bukkit.entity.Player")))
-		{
-		    throw new Error('Player is null.');
-		}
+        if (!(player instanceof Java.type("org.bukkit.entity.Player")))
+        {
+            throw new Error('Player is null.');
+        }
 
-		var inv = player.getInventory();
-		var size = 0;
-		for(var i = 0; i < 36; i++){
-			if (inv.getItem(i) == null) {
-				size += args[0].getMaxStackSize();
-			}else if (inv.getItem(i).isSimilar(args[0])){
-				size += inv.getItem(i).getMaxStackSize() - inv.getItem(i).getAmount();
-			}
+        var inv = player.getInventory();
+        var size = 0;
+        for(var i = 0; i < 36; i++){
+            if (inv.getItem(i) === null) {
+                size += args[0].getMaxStackSize();
+            }else if (inv.getItem(i).isSimilar(args[0])){
+                size += inv.getItem(i).getMaxStackSize() - inv.getItem(i).getAmount();
+            }
 
-			if (size >= args[0].getAmount()) {
-				inv.addItem(args[0]);
-				return;
-			}
-		}
-		throw new Error("Player has no empty slot.");
-	}else{
-		throw new Error("Invalid parameters. Need [ItemStack]");
-	}
+            if (size >= args[0].getAmount()) {
+                inv.addItem(args[0]);
+                return;
+            }
+        }
+        throw new Error("Player has no empty slot.");
+    }else{
+        throw new Error("Invalid parameters. Need [ItemStack]");
+    }
 }
