67b8f796133c95515e41eebeffdf3e2a9fb24aef
Sayakie <sayakie@kakao.com>
2023-07-25 17:26:03 +0900

    chore: update license header

--- bukkit/src/main/resources/Executor/MESSAGE.js
+++ bukkit/src/main/resources/Executor/MESSAGE.js
@@ -1,6 +1,5 @@
 /*******************************************************************************
- *     Copyright (C) 2017 wysohn
- *     Copyright (C) 2022 Ioloolo
+ *     Copyright (c) 2023 TriggerReactor Team
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
