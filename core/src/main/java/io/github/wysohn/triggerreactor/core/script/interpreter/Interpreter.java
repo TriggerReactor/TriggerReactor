21c891543e8363fa58e2d7f6332d315bcd462f80
Sayakie <sayakie@kakao.com>
2023-07-29 21:01:02 +0900

    Revert "refactor: mark operator `:` as deprecated symbol"
    This reverts commit da35008c82e3d79e0a9a5bc07d45252948927b03.

--- core/src/main/java/io/github/wysohn/triggerreactor/core/script/interpreter/Interpreter.java
+++ core/src/main/java/io/github/wysohn/triggerreactor/core/script/interpreter/Interpreter.java
@@ -1408,6 +1408,5 @@ public class Interpreter {
     static {
         Parser.addDeprecationSupervisor(((type, value) -> type == Type.ID && "MODIFYPLAYER".equals(value)));
         Parser.addDeprecationSupervisor(((type, value) -> type == Type.ID && value.contains("$")));
-        Parser.addDeprecationSupervisor(((type, value) -> type == Type.OPERATOR && value.contains(":")));
     }
 }
