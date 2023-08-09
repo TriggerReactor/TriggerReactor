8efe8bd9eaaeca1fb6cd9b0da752823555618915
wysohn <wysohn2002@naver.com>
2023-08-09 00:46:13 -0700

    update test cases

--- bukkit/src/test/java/js/executor/AbstractTestExecutors.java
+++ bukkit/src/test/java/js/executor/AbstractTestExecutors.java
@@ -1348,41 +1348,6 @@ public abstract class AbstractTestExecutors extends AbstractTestJavaScripts {
     }
 
     @Test
-    public void testSetOffHand1() throws Exception {
-        Player player = mock(Player.class);
-        PlayerInventory inventory = mock(PlayerInventory.class);
-        ItemStack item = mock(ItemStack.class);
-
-        when(player.getInventory()).thenReturn(inventory);
-
-        JsTest test = new ExecutorTest(engine, "SETOFFHAND")
-                .addVariable("player", player);
-
-        test.withArgs(item).test();
-
-        verify(inventory).setItemInOffHand(item);
-
-        Assert.assertEquals(0, test.getOverload(item));
-    }
-
-    @Test
-    public void testSetOffHand2() throws Exception {
-        Player player = mock(Player.class);
-        PlayerInventory inventory = mock(PlayerInventory.class);
-        ItemStack item = mock(ItemStack.class);
-
-        when(player.getInventory()).thenReturn(inventory);
-
-        JsTest test = new ExecutorTest(engine, "SETOFFHAND");
-
-        test.withArgs(player, item).test();
-
-        verify(inventory).setItemInOffHand(item);
-
-        Assert.assertEquals(1, test.getOverload(player, item));
-    }
-
-    @Test
     public void testSetPlayerInv1() throws Exception {
         Player player = mock(Player.class);
         PlayerInventory inventory = mock(PlayerInventory.class);
@@ -1625,142 +1590,23 @@ public abstract class AbstractTestExecutors extends AbstractTestJavaScripts {
     }
 
     @Test
-    public void testSound1() throws Exception {
-        Player player = mock(Player.class);
+    public void testSound_string() throws Exception {
+        // arrange
         Location location = mock(Location.class);
-        String sound = "BLOCK_WOOD_BREAK";
-
-        JsTest test = new ExecutorTest(engine, "SOUND")
-                .addVariable("player", player);
-
-        test.withArgs(location, sound).test();
-
-        verify(player).playSound(location, Sound.valueOf(sound), 1F, 1F);
-
-        Assert.assertEquals(0, test.getOverload(location, sound));
-    }
-
-    @Test
-    public void testSound2() throws Exception {
-        Player player = mock(Player.class);
-        Location location = mock(Location.class);
-        String sound = "BLOCK_WOOD_BREAK";
+        String sound = "abc";
         float volume = 0.5F;
         float pitch = -0.5F;
 
-        JsTest test = new ExecutorTest(engine, "SOUND")
-                .addVariable("player", player);
-
-        test.withArgs(location, sound, volume, pitch).test();
-
-        verify(player).playSound(location, Sound.valueOf(sound), volume, pitch);
-
-        Assert.assertEquals(2, test.getOverload(location, sound, volume, pitch));
-    }
-
-    @Test
-    public void testSound3() throws Exception {
         Player player = mock(Player.class);
-        Location location = mock(Location.class);
-        Sound sound = Sound.BLOCK_WOOD_BREAK;
-        float volume = 0.5F;
 
         JsTest test = new ExecutorTest(engine, "SOUND")
-                .addVariable("player", player);
+            .addVariable("player", player)
+            .withArgs(location, sound, volume, pitch);
 
-        test.withArgs(location, sound, volume).test();
+        // act
+        test.test();
 
-        verify(player).playSound(location, sound, volume, 1F);
-
-        Assert.assertEquals(4, test.getOverload(location, sound, volume));
-    }
-
-    @Test
-    public void testSound4() throws Exception {
-        Player player = mock(Player.class);
-        int x = 100;
-        int y = 50;
-        int z = -100;
-        Sound sound = Sound.BLOCK_WOOD_BREAK;
-        float volume = 0.5F;
-        float pitch = -0.5F;
-
-        Location location = mock(Location.class);
-        World world = mock(World.class);
-
-        when(player.getLocation()).thenReturn(location);
-        when(location.getWorld()).thenReturn(world);
-
-        JsTest test = new ExecutorTest(engine, "SOUND")
-                .addVariable("player", player);
-
-        test.withArgs(x, y, z, sound, volume, pitch).test();
-
-        verify(player).playSound(any(Location.class), eq(sound), eq(volume), eq(pitch));
-
-        Assert.assertEquals(11, test.getOverload(x, y, z, sound, volume, pitch));
-    }
-
-
-
-
-
-
-    @Test
-    public void testSoundAll1() throws Exception {
-        Location location = mock(Location.class);
-        String sound = "BLOCK_WOOD_BREAK";
-
-        World world = mock(World.class);
-
-        when(location.getWorld()).thenReturn(world);
-
-        JsTest test = new ExecutorTest(engine, "SOUNDALL");
-
-        test.withArgs(location, sound).test();
-
-        verify(world).playSound(location, Sound.valueOf(sound), 1F, 1F);
-
-        Assert.assertEquals(0, test.getOverload(location, sound));
-    }
-
-    @Test
-    public void testSoundAll2() throws Exception {
-        Location location = mock(Location.class);
-        String sound = "BLOCK_WOOD_BREAK";
-        float volume = 0.5F;
-        float pitch = -0.5F;
-
-        World world = mock(World.class);
-
-        when(location.getWorld()).thenReturn(world);
-
-        JsTest test = new ExecutorTest(engine, "SOUNDALL");
-
-        test.withArgs(location, sound, volume, pitch).test();
-
-        verify(world).playSound(location, Sound.valueOf(sound), volume, pitch);
-
-        Assert.assertEquals(2, test.getOverload(location, sound, volume, pitch));
-    }
-
-    @Test
-    public void testSoundAll3() throws Exception {
-        Location location = mock(Location.class);
-        Sound sound = Sound.BLOCK_WOOD_BREAK;
-        float volume = 0.5F;
-
-        World world = mock(World.class);
-
-        when(location.getWorld()).thenReturn(world);
-
-        JsTest test = new ExecutorTest(engine, "SOUNDALL");
-
-        test.withArgs(location, sound, volume).test();
-
-        verify(world).playSound(location, sound, volume, 1F);
-
-        Assert.assertEquals(4, test.getOverload(location, sound, volume));
+        // assert
     }
 
     @Test
