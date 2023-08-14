33bc412ad1596d920d605ef14645701697b6bb1b
wysohn <wysohn2002@naver.com>
2023-08-13 18:29:06 -0700

    obsolete code

--- core/src/main/java/io/github/wysohn/triggerreactor/core/module/manager/ConfigurationModule.java
+++ core/src/main/java/io/github/wysohn/triggerreactor/core/module/manager/ConfigurationModule.java
@@ -1,16 +1,10 @@
 package io.github.wysohn.triggerreactor.core.module.manager;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import io.github.wysohn.triggerreactor.core.config.source.GsonConfigSource;
 import io.github.wysohn.triggerreactor.core.config.source.IConfigSource;
 import io.github.wysohn.triggerreactor.core.config.source.IConfigSourceFactory;
-import io.github.wysohn.triggerreactor.core.config.source.SaveWorker;
-import io.github.wysohn.triggerreactor.core.main.IExceptionHandle;
-
-import javax.inject.Named;
-import java.io.File;
 
 public class ConfigurationModule extends AbstractModule {
     @Override
@@ -18,15 +12,5 @@ public class ConfigurationModule extends AbstractModule {
         install(new FactoryModuleBuilder()
                 .implement(IConfigSource.class, GsonConfigSource.class)
                 .build(IConfigSourceFactory.class));
-    }
-
-    @Provides
-    @Named("PluginConfig")
-    public IConfigSource providePluginConfigSource(IConfigSourceFactory factory,
-                                                   @Named("DataFolder") File dataFolder,
-                                                   IExceptionHandle exceptionHandle) {
-        return factory.create(new SaveWorker(5, (ex) -> exceptionHandle.handleException(null, ex)),
-                dataFolder,
-                "config");
     }
 }
