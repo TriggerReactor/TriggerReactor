175e2969bf3849fea9536cc72f22dda26d83dbcc
Cupelt <guswnd8800@naver.com>
2023-09-20 09:42:09 +0900

    add internal-ver "this"

--- core/src/main/java/io/github/wysohn/triggerreactor/core/manager/trigger/Trigger.java
+++ core/src/main/java/io/github/wysohn/triggerreactor/core/manager/trigger/Trigger.java
@@ -185,6 +185,7 @@ public abstract class Trigger implements Cloneable, IObservable {
             return false;
         }
 
+        scriptVars.put("this", this);
         scriptVars.put("event", e);
         scriptVars.putAll(triggerDependencyFacade.getExtraVariables(e));
 
