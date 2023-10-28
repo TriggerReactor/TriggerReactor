3a6538847c90dac57face70326c14224a56db993
wysohn <wysohn2002@naver.com>
2023-10-28 16:45:24 -0700

    new method added

--- core/src/main/java/io/github/wysohn/triggerreactor/core/Constants.java
+++ core/src/main/java/io/github/wysohn/triggerreactor/core/Constants.java
@@ -3,5 +3,5 @@ package io.github.wysohn.triggerreactor.core;
 public class Constants {
     public static final String PERMISSION = "triggerreactor.admin";
 
-
+    public static final String PERMISSION_SEE_EXCEPTIONS = "triggerreactor.see.exceptions";
 }
